2ac5b7dd5db07e2f382ac4f04eecc203f5688c70
[Hasbro] <haseeb@galapagos-20.cs.wisc.edu>
2015-01-30 23:00:38 -0600

    pipe.c ready for multiple runs of specified size

--- pipe.c
+++ pipe.c
@@ -10,24 +10,29 @@
 #define K 1024
 #define READ 0
 #define WRITE 1
+#define MAX_uint64 0xFFFFFFFFFFFFFFFF
 
 int main(int argc, char* argv[])
 {
-	/*
-	//Experiment variables
-	int sizes[]={4, 16, 64, 256, 512, 1*K, 4*K, 16*K, 64*K, 256*K, 512*K};
-	char* msg;
 
-	//number of times experiment runs
-	for(int i=0; i<sizeof(sizes)/sizeof(sizes[0]); i++)
+	if(argc!=3)
 	{
+		perror("Usage:./pipe [runs] [size]\n");
+		exit(1);
 	}
-*/
+
+	int runs=atoi(argv[1]);
+	int size=atoi(argv[2]);
+	char* msg= (char*) malloc(size);
+	memset(msg, '0', size);
+	char readBuff[size];
 
 	//Timer variables
-	uint64_t diffNS;//in nano seconds
+	uint64_t diffNS,minNS;//in nano seconds
+	minNS=MAX_uint64;
 	struct timespec start, end;
 
+/*
 	//rdtsc	
 	unsigned long long a,b;
 	//Get frequency of processor
@@ -35,12 +40,11 @@ int main(int argc, char* argv[])
 	sleep(2);
 	b=rdtsc();
 	unsigned long long rdtsFreq= (b-a)/2;
-
+*/
 
 	int sendPipe[2], ackPipe[2];
 	int childpid;
-	char msg[]="Hello\0";
-	char readBuff[80];
+
 
 	if( pipe(sendPipe)!=0 || pipe(ackPipe)!=0)
 	{
@@ -60,12 +64,18 @@ int main(int argc, char* argv[])
 		close(sendPipe[WRITE]);
 		close(ackPipe[READ]);
 
-		//Child revieves message from parent
-		read(sendPipe[READ], readBuff, sizeof(readBuff));
-		//Child sends reply to parent
-		write(ackPipe[WRITE], msg, strlen(msg)+1);
-
-		//printf("Child recieved parent msg: %s\n",readBuff);
+		while(runs>0)
+		{
+			//Child revieves message from parent
+			read(sendPipe[READ], readBuff, sizeof(readBuff));
+			
+			//printf("Child recieved parent msg: %s\n",readBuff);
+			
+			//Child sends reply to parent
+			write(ackPipe[WRITE], msg, strlen(msg)+1);
+		
+			runs--;
+		}
 
 	}
 	else
@@ -74,27 +84,36 @@ int main(int argc, char* argv[])
 		close(sendPipe[READ]);
 		close(ackPipe[WRITE]);
 
-		//start timer
-		//clock_gettime(CLOCK_MONOTONIC, &start);
-		a=rdtsc();	
+		while(runs>0)
+		{
+			//start timer
+			clock_gettime(CLOCK_MONOTONIC, &start);
+			//a=rdtsc();	
 
-		//Parent sends message to child
-		write(sendPipe[WRITE], msg, strlen(msg)+1);
-		//Parent recieves message from child
-		read(ackPipe[READ], readBuff, sizeof(readBuff));
+			//Parent sends message to child
+			write(sendPipe[WRITE], msg, strlen(msg)+1);
+			//Parent recieves message from child
+			read(ackPipe[READ], readBuff, sizeof(readBuff));
 
 
-		//stop timer
-		//clock_gettime(CLOCK_MONOTONIC, &end);
-		b=rdtsc();
-	
-	
-		//calculate difference	
-		//diffNS = 1e9L * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-		double diffNS= (double) (1e9*( 1.0*(b-a)/rdtsFreq) );
+			//stop timer
+			clock_gettime(CLOCK_MONOTONIC, &end);
+			//b=rdtsc();
+		
+		
+			//calculate difference	
+			diffNS = 1e9L * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+			//double diffNS= (double) (1e9*( 1.0*(b-a)/rdtsFreq) );
 
-		printf("Parent recieved reply: %s\n",readBuff);	
-		printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diffNS);
+			printf("Parent recieved reply: %s\n",readBuff);	
+			printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diffNS);
+
+			if(minNS>diffNS)
+				minNS=diffNS;
+			runs--;
+		}
+		
+		printf("Min elapsed time = %llu nanoseconds\n", (long long unsigned int) minNS);
 
 	}
 
